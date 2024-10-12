3266ead384fb43b2efc09a3fd116648445c4a27d
wramirez09 <wramirez1980@gmail.com>
2024-10-12 09:09:23 -0500

    update config

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"gCXQjvXcwHV1JmPqIjhOSZ8mhW8EV+gpQ2mkCU3OurQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/app/blog-sidebar/page-63c78d5eb66c4635.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[249],{1953:function(e,n,s){Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,9521))}},function(e){e.O(0,[250,749,521,971,69,744],function(){return e(e.s=1953)}),_N_E=e.O()}]);
