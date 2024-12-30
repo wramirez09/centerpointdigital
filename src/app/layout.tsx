7ec4256f170b690c781c3054431b851803d9a578
Will Ramirez <wramirez1980@gmail.com>
2024-12-30 17:55:32 -0600

    added google analaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import ScrollToTop from '@/components/ScrollToTop';
 import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import 'node_modules/react-modal-video/css/modal-video.css';
 import '../styles/index.css';
 
@@ -22,8 +23,11 @@ export default function RootLayout({
       */}
       <head>
         <ColorSchemeScript defaultColorScheme="auto" />
+        <GoogleAnalytics gaId="G-K5DQ259S4B" />
       </head>
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className} w-full h-full overflow-y-scroll`}>
+      <body
+        className={`bg-[#FCFCFC] dark:bg-black ${inter.className} w-full h-full overflow-y-scroll`}
+      >
         <Providers>
           <Header />
           {children}
