f8ca4794f5cc8f03469ae48334470e848dd69eaa
Will Ramirez <wramirez1980@gmail.com>
2025-01-01 17:47:29 -0600

    chore: added form functionality

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,6 +3,9 @@ import { motion } from 'framer-motion';
 import NewsLatterBox from './NewsLetterBox';
 import { Container, Box, Text, Stack } from '@mantine/core';
 import { IconAt, IconMapPin, IconPhone, IconSun } from '@tabler/icons-react';
+import emailjs from '@emailjs/browser';
+import { useEffect, useState } from 'react';
+import { RotatingLines } from 'react-loader-spinner';
 
 function printHref(title: string, description: string): string {
   const prefixMap: Record<string, string> = {
@@ -76,11 +79,67 @@ type ContactProps = {
   copy?: string;
 };
 
+type FormValues = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const defaultFormValues = {
+  name: '',
+  email: '',
+  message: '',
+};
+
 const Contact: React.FC<ContactProps> = ({
   showNewsLetter = true,
   header = defaultContent.header,
   copy = defaultContent.copy,
 }) => {
+  useEffect(() => {
+    emailjs.init({
+      publicKey: '1Y7wGEfTmPGt-GE90',
+      // Do not allow headless browsers
+      blockHeadless: true,
+      blockList: {
+        // Block the suspended emails
+        list: [],
+        // The variable contains the email address
+        watchVariable: 'userEmail',
+      },
+      limitRate: {
+        // Set the limit rate for the application
+        id: 'app',
+        // Allow 1 request per 10s
+        throttle: 10000,
+      },
+    });
+  }, []);
+
+  const [formValues, setFormValues] = useState<FormValues>(defaultFormValues);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function sendEmail() {
+    console.log(formValues);
+    if (formValues.name !== '') {
+      setIsLoading(true);
+      emailjs
+        .send('default_service', 'template_teh6b2b', {
+          name: formValues.name,
+          email: formValues.email,
+          message: formValues.message,
+        })
+        .then(
+          response => {
+            setIsLoading(false);
+            setFormValues(defaultFormValues);
+          },
+          error => {
+            console.log('FAILED...', error);
+          },
+        );
+    }
+  }
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -109,61 +168,93 @@ const Contact: React.FC<ContactProps> = ({
                     discuss how we can help your business grow, our team is here
                     to assist you. Reach out to us through the form below.
                   </p>
-                  <form>
-                    <div className="-mx-4 flex flex-wrap">
-                      <div className="w-full px-4 md:w-1/2">
-                        <div className="mb-8">
-                          <label
-                            htmlFor="name"
-                            className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                  {!isLoading ? (
+                    <form id="contactForm">
+                      <div className="-mx-4 flex flex-wrap">
+                        <div className="w-full px-4 md:w-1/2">
+                          <div className="mb-8">
+                            <label
+                              htmlFor="name"
+                              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                            >
+                              Your Name
+                            </label>
+                            <input
+                              onChange={event =>
+                                setFormValues({
+                                  ...formValues,
+                                  name: event.target.value,
+                                })
+                              }
+                              value={formValues.name}
+                              name="name"
+                              type="text"
+                              placeholder="Enter your name"
+                              className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                            />
+                          </div>
+                        </div>
+                        <div className="w-full px-4 md:w-1/2">
+                          <div className="mb-8">
+                            <label
+                              htmlFor="email"
+                              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                            >
+                              Your Email
+                            </label>
+                            <input
+                              onChange={event =>
+                                setFormValues({
+                                  ...formValues,
+                                  email: event.target.value,
+                                })
+                              }
+                              value={formValues.email}
+                              name="email"
+                              type="email"
+                              placeholder="Enter your email"
+                              className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                            />
+                          </div>
+                        </div>
+                        <div className="w-full px-4">
+                          <div className="mb-8">
+                            <label
+                              htmlFor="message"
+                              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                            >
+                              Your Message
+                            </label>
+                            <textarea
+                              value={formValues.message}
+                              onChange={event =>
+                                setFormValues({
+                                  ...formValues,
+                                  message: event.target.value,
+                                })
+                              }
+                              name="message"
+                              rows={5}
+                              placeholder="Enter your Message"
+                              className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                            ></textarea>
+                          </div>
+                        </div>
+                        <div className="w-full px-4">
+                          <button
+                            className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
+                            formAction={sendEmail}
                           >
-                            Your Name
-                          </label>
-                          <input
-                            type="text"
-                            placeholder="Enter your name"
-                            className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                          />
+                            Submit
+                          </button>
                         </div>
                       </div>
-                      <div className="w-full px-4 md:w-1/2">
-                        <div className="mb-8">
-                          <label
-                            htmlFor="email"
-                            className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                          >
-                            Your Email
-                          </label>
-                          <input
-                            type="email"
-                            placeholder="Enter your email"
-                            className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                          />
-                        </div>
-                      </div>
-                      <div className="w-full px-4">
-                        <div className="mb-8">
-                          <label
-                            htmlFor="message"
-                            className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                          >
-                            Your Message
-                          </label>
-                          <textarea
-                            name="message"
-                            rows={5}
-                            placeholder="Enter your Message"
-                            className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                          ></textarea>
-                        </div>
-                      </div>
-                      <div className="w-full px-4">
-                        <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                          Submit
-                        </button>
-                      </div>
+                    </form>
+                  ) : (
+                    <div className="flex justify-center items-center">
+                      <RotatingLines strokeColor="white" width="50px" />
                     </div>
-                  </form>
+                  )}
                 </div>
               </div>
               {showNewsLetter ? (
